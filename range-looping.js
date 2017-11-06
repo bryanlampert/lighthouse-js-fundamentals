dbc90c2fa9bf07defac404a9c78ea50612b47a73
bryanlampert <bryanlampert@gmail.com>
2017-11-06 04:01:46 +0000

    Adds looping range  exercise

---
+++ range-looping.js
@@ -0,0 +1,16 @@
+function range(start, end, step) {
+  var arrayVar = [];
+  if (step < 0 || start > end || start === undefined || end === undefined || step === undefined) {
+    return arrayVar;
+  } else {
+    while (start <= end) {
+      arrayVar.push(start);
+      start += step;
+    }
+    return arrayVar;
+  }
+}
+
+console.log(range(0, 10, 2));
+console.log(range(10, 30, 5));
+console.log(range(-5, 2, 3));
